17ebddefb38d10b56d2682c533589ebaea9949fa
Leznyov Artyom <a-leznev@mail.ru>
2024-08-17 21:31:55 +0700

    Add parameterization to one test in test_product_page.py

--- test_product_page.py
+++ test_product_page.py
@@ -1,8 +1,12 @@
 from .pages.product_page import ProductPage
+import pytest
 
 
-def test_guest_can_add_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+product_base_link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
+urls = [f"{product_base_link}/?promo=offer{num}" if num != 7 else pytest.param(f"{product_base_link}/?promo=offer{num}", marks=pytest.mark.xfail) for num in range(10)]
+
+@pytest.mark.parametrize('link', urls)
+def test_guest_can_add_product_to_basket(browser, link):
     page = ProductPage(browser, link)
     page.open()
     page.add_product_to_basket()
