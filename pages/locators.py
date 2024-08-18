99d809f626a0bc3859dacb14dfa3141387da78c0
Leznyov Artyom <a-leznev@mail.ru>
2024-08-18 13:43:39 +0700

    Add test about empty basket on basket page

--- pages/locators.py
+++ pages/locators.py
@@ -4,6 +4,12 @@ from selenium.webdriver.common.by import By
 class BasePageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
     LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+    BASKET_LINK = (By.CSS_SELECTOR, ".basket-mini a.btn.btn-default")
+
+
+class BasketPageLocators():
+    PRODUCT_IN_BASKET = (By.CSS_SELECTOR, "#basket_formset .basket-items")
+    EMPTY_BASKET = (By.CSS_SELECTOR, "#content_inner > p")
 
 
 class LoginPageLocators():
