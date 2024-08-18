839f4770406a9a97ac054cfb929749e11bcf8116
Leznyov Artyom <a-leznev@mail.ru>
2024-08-18 12:22:51 +0700

    Transfer methods from main_page to base_page and add tests to test_product_page

--- pages/base_page.py
+++ pages/base_page.py
@@ -2,6 +2,7 @@ from selenium.common.exceptions import NoSuchElementException, NoAlertPresentExc
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import math
+from .locators import BasePageLocators
 
 
 class BasePage():
@@ -12,8 +13,19 @@ class BasePage():
         self.browser.implicitly_wait(timeout)
 
 
-    def open(self):
-        self.browser.get(self.url)
+    def go_to_login_page(self):
+        login_link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
+        login_link.click()
+
+
+    # Элемент исчезает в течение заданного количества времени
+    def is_disappeared(self, how, what, timeout=4):
+        try:
+            WebDriverWait(self.browser, timeout, 1, TimeoutException).until_not(EC.presence_of_element_located((how, what)))
+        except TimeoutException:
+            return False
+
+        return True
 
 
     def is_element_present(self, how, what):
@@ -22,6 +34,24 @@ class BasePage():
         except NoSuchElementException:
             return False
         return True
+    
+
+    # Элемент не появляется на странице в течение заданного количества времени
+    def is_not_element_present(self, how, what, timeout=4):
+        try:
+            WebDriverWait(self.browser, timeout).until(EC.presence_of_element_located((how, what)))
+        except TimeoutException:
+            return True
+
+        return False
+    
+
+    def open(self):
+        self.browser.get(self.url)
+    
+
+    def should_be_login_link(self):
+        assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
 
 
     def solve_quiz_and_get_code(self):
@@ -37,23 +67,3 @@ class BasePage():
             alert.accept()
         except NoAlertPresentException:
             print("No second alert presented")
-
-
-    # Элемент не появляется на странице в течение заданного количества времени
-    def is_not_element_present(self, how, what, timeout=4):
-        try:
-            WebDriverWait(self.browser, timeout).until(EC.presence_of_element_located((how, what)))
-        except TimeoutException:
-            return True
-
-        return False
-    
-
-    # Элемент исчезает в течение заданного количества времени
-    def is_disappeared(self, how, what, timeout=4):
-        try:
-            WebDriverWait(self.browser, timeout, 1, TimeoutException).until_not(EC.presence_of_element_located((how, what)))
-        except TimeoutException:
-            return False
-
-        return True
--- pages/main_page.py
+++ pages/main_page.py
@@ -1,13 +1,6 @@
 from .base_page import BasePage
-from .locators import MainPageLocatots
 
 
 class MainPage(BasePage):
-
-    def go_to_login_page(self):
-        login_link = self.browser.find_element(*MainPageLocatots.LOGIN_LINK)
-        login_link.click()
-
-    
-    def should_be_login_link(self):
-        assert self.is_element_present(*MainPageLocatots.LOGIN_LINK), "Login link is not presented"
+    def __init__(self, *args, **kwargs):
+        super(MainPage, self).__init__(*args, **kwargs)
